e68e9b4484f4a4f20cdda346268cf48187eae15a
Chekhov <pavel.miridonov123@gmail.com>
2025-05-29 15:28:25 +0400

    Task 1

---
+++ index.js
@@ -0,0 +1,16 @@
+// Task 1
+// 2619. Array Prototype Last
+
+// /**
+//  * @return {null|boolean|number|string|Array|Object}
+//  */
+// Array.prototype.last = function () {
+//     return this.length > 0 ? this[this.length - 1] : -1
+// };
+//
+// /**
+//  * const arr = [1, 2, 3];
+//  * arr.last(); // 3
+//  */
+// const arr = [1, 2, 3]
+// console.log(arr.last());
