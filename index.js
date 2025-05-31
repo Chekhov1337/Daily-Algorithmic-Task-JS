43f3c6eee900d0cdd45ef71b015a99c2a8b59998
Chekhov <pavel.miridonov123@gmail.com>
2025-05-31 20:11:08 +0400

    Task 4

--- index.js
+++ index.js
@@ -40,16 +40,51 @@
 // Task 3
 // 2621. Sleep
 
+// /**
+//  * @param {number} millis
+//  * @return {Promise}
+//  */
+// async function sleep(millis) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {resolve()
+//         }, millis)
+//     })
+// }
+//
+// let t = Date.now()
+// sleep(1000).then(() => console.log(Date.now() - t)) // 100
+
+
+// Task 4
+// 2626. Array Reduce Transformation
+
 /**
- * @param {number} millis
- * @return {Promise}
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
  */
-async function sleep(millis) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {resolve()
-        }, millis)
+// var reduce = function (nums, fn, init) {
+//     let accum = init
+//
+//     for (let i = 0; i < nums.length; i++) {
+//         accum = fn(accum, nums[i])
+//     }
+//     return accum
+// }
+
+var reduce = function (nums, fn, init) {
+    let accum = init
+
+    nums.forEach((element) => {
+        accum = fn(accum, element)
     })
+    return accum
 }
 
-let t = Date.now()
-sleep(1000).then(() => console.log(Date.now() - t)) // 100
+let res = reduce([1, 2, 3, 4], function sum(accum, curr) {
+    return accum + curr
+}, 0)
+
+console.log(res)
+
